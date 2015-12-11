459aa6fab137a4b4f57b1f31e280964bdec2eb61
okinawaopenlabs <ool-github@okinawaopenlabs.org>
2015-12-11 20:57:14 +0900

    modify readDeviceList

--- src/main/java/org/okinawaopenlabs/ofpm/business/DeviceBusinessImpl.java
+++ src/main/java/org/okinawaopenlabs/ofpm/business/DeviceBusinessImpl.java
@@ -369,7 +369,14 @@ public class DeviceBusinessImpl implements DeviceBusiness {
 				dev.setDeviceName((String) infoMap.get("name"));
 				dev.setDeviceType((String) infoMap.get("type"));
 				dev.setDatapathId((String) infoMap.get("datapathId"));
-				dev.setOfcIp((String) infoMap.get("ofcIp"));
+				dev.setLocation((String) infoMap.get("location"));
+				dev.setTenant((String) infoMap.get("tenant"));
+				if(infoMap.containsKey("ip") && infoMap.containsKey("port")){
+					dev.setOfcIp(((String)infoMap.get("ip") + ":" + (String)infoMap.get("port").toString()));
+				}
+//				StringBuilder str_buil = new StringBuilder();
+//				str_buil.append((String) infoMap.get("ip"));
+//				str_buil.append((String) infoMap.get("port"));
 				result.add(dev);
 			}
 
